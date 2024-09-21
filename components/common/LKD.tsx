7283a7325eac41918bed4c3e37a0dcea04ec8476
Stepen <histepen@gmail.com>
2024-09-21 11:42:48 +0800

    refactor: implement interface

--- components/common/LKD.tsx
+++ components/common/LKD.tsx
@@ -1,4 +1,10 @@
-const LKD = ({ children }) => {
+import { ReactNode } from 'react';
+
+interface LKDProps {
+  children: ReactNode;
+}
+
+const LKD = ({ children }: LKDProps) => {
   return <span className='rounded border border-slate-500 bg-slate-700 text-slate-300 px-1 py-[2px]'>{children}</span>;
 };
 
